5d19546ba89f8281626e8144e778751d9374a916
Muhammad Haseeb <mhaseeb98070@gmail.com>
2026-07-27 20:17:20 +0500

    Fix backend lazy MongoDB initialization for Vercel serverless functions

--- backend-books/app/database/mongodb.py
+++ backend-books/app/database/mongodb.py
@@ -8,20 +8,28 @@ db = None
 async def connect_to_mongo():
     global client, database, db
 
-    client = AsyncIOMotorClient(settings.MONGODB_URI)
-    database = client[settings.DATABASE_NAME]
-    db = database
-
-    print("Connected to MongoDB")
+    if client is None:
+        client = AsyncIOMotorClient(settings.MONGODB_URI)
+        database = client[settings.DATABASE_NAME]
+        db = database
+        print("Connected to MongoDB")
 
 
 async def close_mongo_connection():
-    global client
+    global client, database, db
 
     if client:
         client.close()
+        client = None
+        database = None
+        db = None
         print("MongoDB connection closed")
 
 
 def get_database():
+    global client, database, db
+    if database is None:
+        client = AsyncIOMotorClient(settings.MONGODB_URI)
+        database = client[settings.DATABASE_NAME]
+        db = database
     return database

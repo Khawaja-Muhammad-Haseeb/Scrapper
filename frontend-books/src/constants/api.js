817e51919659727ce262720afbbe85674ffb8f2e
Muhammad Haseeb <mhaseeb98070@gmail.com>
2026-07-27 20:08:45 +0500

    Fixed Deployment

--- frontend-books/src/constants/api.js
+++ frontend-books/src/constants/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api'
+export const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://booksitebackend.vercel.app/api'
 
 export const API_ENDPOINTS = {
   AUTH: {

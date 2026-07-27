85ef3240563a41889bc245556fed64005d6cf5d3
Muhammad Haseeb <mhaseeb98070@gmail.com>
2026-07-27 20:18:04 +0500

    Update default MONGODB_URI to MongoDB Atlas and ALLOWED_ORIGINS to wildcard

--- backend-books/app/config/settings.py
+++ backend-books/app/config/settings.py
@@ -4,14 +4,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     APP_NAME: str = "BookScrape API"
 
-    MONGODB_URI: str = "mongodb://localhost:27017"
+    MONGODB_URI: str = "mongodb+srv://m3050413_db_user:nEYDnPK9mf4qWJx9@books.ctmaz1o.mongodb.net/?appName=Books"
     DATABASE_NAME: str = "books_toscrape"
 
     JWT_SECRET_KEY: str = "mysecretkey123456789"
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
-    ALLOWED_ORIGINS: str = "http://localhost:5173,http://localhost:3000,http://127.0.0.1:5173"
+    ALLOWED_ORIGINS: str = "*"
 
     model_config = SettingsConfigDict(
         env_file=".env",
